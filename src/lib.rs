a621cfc004ff3319cd76ab7b9c1d2f90b7bcb6e1
Wouter Breedveld <W.J.Breedveld-1@student.tudelft.nl>
2023-05-04 13:47:41 +0200

    FIll lib with all functions for example

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+use std::ffi::{c_char, c_ulong, c_ushort};
+
 pub mod sys;
 
 pub fn NVENCAPI_STRUCT_VERSION(ver: i32) -> i32 {
@@ -9,7 +11,10 @@ pub fn NVENCAPI_STRUCT_VERSION(ver: i32) -> i32 {
 mod tests {
     use crate::{sys::nvEncodeAPI::*, NVENCAPI_STRUCT_VERSION};
     use cudarc::driver::sys::*;
-    use std::{ffi::c_int, ffi::{c_void, c_char, c_uint}};
+    use std::{
+        ffi::c_int,
+        ffi::{c_char, c_uint, c_void},
+    };
 
     #[test]
     fn example() {
@@ -68,11 +73,17 @@ mod tests {
             let mut cuDevice = 0;
             cuDeviceGet(&mut cuDevice as CUdevice, iGpu as c_int);
             let mut szDeviceName: [c_char; 80];
-            cuDeviceGetName(&mut szDeviceName as *mut c_char, szDeviceName.len() as c_int, cuDevice as CUdevice);
+            cuDeviceGetName(
+                &mut szDeviceName as *mut c_char,
+                szDeviceName.len() as c_int,
+                cuDevice as CUdevice,
+            );
             let mut cuContext = std::ptr::null::<c_void>();
-            cuCtxCreate_v2(&mut cuContext as *mut CUcontext, 0 as c_uint, cuDevice as CUdevice);
-            
-            
+            cuCtxCreate_v2(
+                &mut cuContext as *mut CUcontext,
+                0 as c_uint,
+                cuDevice as CUdevice,
+            );
 
             NvEncodeAPICreateInstance(&mut function_list as *mut NV_ENCODE_API_FUNCTION_LIST);
 
@@ -89,15 +100,177 @@ mod tests {
             };
 
             let nvEncOpenEncodeSessionEx = function_list.nvEncOpenEncodeSessionEx.unwrap();
-            let status = nvEncOpenEncodeSessionEx(&mut session_params as *mut _NV_ENC_OPEN_ENCODE_SESSIONEX_PARAMS, &mut cuContext as *mut *mut c_void);
+            let status = nvEncOpenEncodeSessionEx(
+                &mut session_params as *mut _NV_ENC_OPEN_ENCODE_SESSIONEX_PARAMS,
+                &mut cuContext as *mut *mut c_void,
+            );
 
             if status != _NVENCSTATUS::NV_ENC_SUCCESS {
                 //If the creation of encoder session fails, the client must call ::NvEncDestroyEncoder API before exiting.
             }
 
             let nvEncGetEncodeGUIDCount = function_list.nvEncGetEncodeGUIDCount.unwrap();
-            let mut supportedGuidsCount:u32 = 0;
-            let _res = nvEncGetEncodeGUIDCount(&mut function_list as *mut c_void, &mut supportedGuidsCount as *mut u32);
+            let mut supportedGuidsCount: u32 = 0;
+            let _ = nvEncGetEncodeGUIDCount(
+                &mut function_list as *mut c_void,
+                &mut supportedGuidsCount as *mut u32,
+            );
+
+            let nvEncGetEncodeGUIDs = function_list.nvEncGetEncodeGUIDs.unwrap();
+            let mut GUIDCount: u32 = 0;
+            let mut encodeGUIDs = vec![GUID; supportedGuidsCount];
+            nvEncGetEncodeGUIDs(
+                &mut function_list as *mut c_void,
+                encodeGUIDs.as_mut_ptr() as *mut _GUID,
+                supportedGuidsCount,
+                &mut GUIDCount as *mut u32,
+            );
+            //TODO: check using GUIDCount
+            let encodeGUID = encodeGUIDs
+                .first()
+                .expect("There should be at least 1 encode GUID");
+
+            let nvEncGetEncodePresetCount = function_list.nvEncGetEncodePresetCount.unwrap();
+            let mut encodePresetGUIDCount: u32 = 0;
+            nvEncGetEncodePresetCount(
+                &mut function_list as *mut c_void,
+                encodeGUID,
+                &mut encodePresetGUIDCount as *mut u32,
+            );
+
+            let nvEncGetEncodePresetGUIDs = function_list.nvEncGetEncodePresetGUIDs.unwrap();
+            let mut guidArraySize: u32 = 0;
+            let mut presetGUIDs = vec![GUID; encodePresetGUIDCount];
+            nvEncGetEncodePresetGUIDs(
+                &mut function_list as *mut c_void,
+                encodeGUID,
+                presetGUIDs.as_mut_ptr() as *mut _GUID,
+                encodePresetGUIDCount,
+                &mut guidArraySize as *mut u32,
+            );
+
+            //TODO: check using guidArraySize
+            let presetGUID = presetGUIDs
+                .first()
+                .expect("There should be at least 1 preset GUID");
+
+            let nvEncGetEncodePresetConfigEx = function_list.nvEncGetEncodePresetConfigEx.unwrap();
+            let mut presetConfig = NV_ENC_PRESET_CONFIG;
+            nvEncGetEncodePresetConfigEx(
+                &mut function_list as *mut c_void,
+                encodeGUID,
+                presetGUID,
+                NV_ENC_TUNING_INFO::NV_ENC_TUNING_INFO_LOW_LATENCY,
+                &mut presetConfig as *mut _NV_ENC_PRESET_CONFIG,
+            );
+
+            let nvEncInitializeEncoder = function_list.nvEncInitializeEncoder.unwrap();
+            nvEncInitializeEncoder(
+                &mut function_list as *mut c_void,
+                &mut presetConfig as *mut _NV_ENC_PRESET_CONFIG,
+            );
+
+            //Encoding the stream
+            //Allocate buffer
+            let nvEncRegisterResource = function_list.nvEncRegisterResource.unwrap();
+            //TODO set resource params
+            let mut registerResParams = NV_ENC_REGISTER_RESOURCE {};
+            nvEncRegisterResource(
+                &mut function_list as *mut c_void,
+                &mut registerResParams as *mut _NV_ENC_REGISTER_RESOURCE,
+            );
+
+            let nvEncMapInputResource = function_list.nvEncMapInputResource.unwrap();
+            //TODO check if param is correct
+            let mapInputResParams = NV_ENC_MAP_INPUT_RESOURCE {
+                registeredResource: registerResParams.registeredResource,
+            };
+            nvEncMapInputResource(
+                &mut function_list as *mut c_void,
+                &mut mapInputResParams as *mut _NV_ENC_MAP_INPUT_RESOURCE,
+            );
+
+            //Submitting
+            //TODO set pic params
+            let mut picParams = NV_ENC_PIC_PARAMS {
+                inputBuffer: mapInputResParams.mappedResource,
+            };
+
+            let nvEncEncodePicture = function_list.nvEncEncodePicture.unwrap();
+            nvEncEncodePicture(
+                &mut function_list as *mut c_void,
+                &mut picParams as *mut _NV_ENC_PIC_PARAMS,
+            );
+
+            let nvEncLockBitstream = function_list.nvEncLockBitstream.unwrap();
+            //TODO set bitstream buffer params
+            let mut lockBitstreamBufferParams = NV_ENC_LOCK_BITSTREAM;
+            nvEncLockBitstream(
+                &mut function_list as *mut c_void,
+                &mut lockBitstreamBufferParams as *mut _NV_ENC_LOCK_BITSTREAM,
+            );
+
+            // Start destroy
+            let nvEncUnmapInputResource = function_list.nvEncUnmapInputResource.unwrap();
+            nvEncUnmapInputResource(&mut function_list as *mut c_void);
+
+            let nvEncUnregisterResource = function_list.nvEncUnregisterResource.unwrap();
+            nvEncUnregisterResource(
+                &mut function_list as *mut c_void,
+                &mut registerResParams.registeredResource,
+            );
+
+            let mut picParamsEnd = NV_ENC_PIC_PARAMS {
+                version: 0,
+                inputWidth: 0,
+                inputHeight: 0,
+                inputPitch: 0,
+                encodePicFlags: NV_ENC_PIC_FLAGS::NV_ENC_PIC_FLAG_EOS,
+                frameIdx: 0,
+                inputTimeStamp: 0,
+                inputDuration: 0,
+                inputBuffer: 0,
+                outputBitstream: 0,
+                completionEvent: 0,
+                bufferFmt: 0,
+                pictureStruct: 0,
+                pictureType: 0,
+                codecPicParams: 0,
+                meHintCountsPerBlock: 0,
+                meExternalHints: 0,
+                reserved1: 0,
+                reserved2: 0,
+                qpDeltaMap: 0,
+                qpDeltaMapSize: 0,
+                reservedBitFields: 0,
+                meHintRefPicDist: 0,
+                alphaBuffer: 0,
+                meExternalSbHints: 0,
+                meSbHintsCount: 0,
+                reserved3: 0,
+                reserved4: 0,
+            };
+
+            nvEncEncodePicture(
+                &mut function_list as *mut c_void,
+                &mut picParamsEnd as *mut _NV_ENC_PIC_PARAMS,
+            );
+
+            let nvEncUnlockBitstream = function_list.nvEncUnlockBitstream.unwrap();
+            nvEncUnlockBitstream(
+                &mut function_list as *mut c_void,
+                &mut lockBitstreamBufferParams as *mut c_void,
+            );
+
+            let nvEncDestroyBitstreamBuffer = function_list.nvEncDestroyBitstreamBuffer.unwrap();
+            // Not clear if there parameters should be lockBitstreamBufferParams or lockBitstreamBufferParams.bitstreamBufferPtr
+            nvEncDestroyBitstreamBuffer(
+                &mut function_list as *mut c_void,
+                &mut lockBitstreamBufferParams as *mut c_void,
+            );
+
+            let nvEncDestroyEncoder = function_list.nvEncDestroyEncoder.unwrap();
+            nvEncDestroyEncoder(&mut function_list as *mut c_void);
         }
     }
 }
